329da4eb6571dd9542ba61f5c485c22bc5626505
Syed Ali Asghar <aligee512@gmail.com>
2025-11-16 17:25:39 +0500

    Frontend Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "../src/App.css"
+import React from 'react';
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import Store from "../src/redux/store.js"
+import useUserRoutes from "./components/routes/userRoutes.jsx";
+import useAdminRoutes from "./components/routes/adminRoutes.jsx";
+import NotFound from "./components/layouts/NotFound.jsx";
+import BottomNavBar from "./components/layouts/lownav.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const userRoutes = useUserRoutes()
+  const adminRoutes = useAdminRoutes()
+
+    return (
+      <>
+      
+        <Provider store={Store}>
+        <Router>
+        <Toaster position="top-center"/>
+          <div className="App">
+            <Header />
+            
+            <div className="container">
+              <Routes>
+               {userRoutes}
+               {adminRoutes}
+               <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <BottomNavBar />
+            <Footer />
+          </div>
+        </Router>
+        </Provider>
+        </>
+    )
+
 }
 
+
 export default App;
+
